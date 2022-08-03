26e46d77ae4d75ff0193bf6e815164141537b07d
Swin <guerrajonmichael@gmail.com>
2022-08-02 19:12:57 -0500

    stuff

--- assets/js/game.js
+++ assets/js/game.js
@@ -62,11 +62,24 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 }
 }
 }
-
 for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+    
     var pickedEnemyName = enemyNames[i];
+
     enemyHealth = 50;
     fight(pickedEnemyName);
 }
+
+else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+}
+
+}
+
+    
+   
 
 //fight();
